b0c34190c7129f1b7614965895a6c686eb3da845
catatonic2021 <83355378+jcarter406@users.noreply.github.com>
2022-03-30 09:29:57 -0600

    Add files via upload

---
+++ week 11 class model/js/sketchModel.js
@@ -0,0 +1,35 @@
+//3D model upload for class demo Mar 30
+//Using coding train example to prepare demo code
+
+let angle = 50;
+let fur;
+let cat;
+
+function preload() 
+{
+    fur = loadImage('assets/globe.png');
+    cat = loadModel('assets/cat.obj', true);
+}
+
+function setup()
+{
+    createCanvas(600, 600, WEBGL);
+}
+
+function draw()
+{
+    background(30, 144, 255);
+    scale(2);
+    ambientLight(255);
+    directionalLight(255, 255, 255, 0, 0, 1);
+    rotateX(angle * 0.1);
+    rotateY(angle * 0.1);
+    rotateZ(angle * 0.1);
+    translate(20, 20, 20);
+    //normalMaterial;
+    texture(fur);
+    model(cat);
+    angle += 0.03;
+}
+
+
